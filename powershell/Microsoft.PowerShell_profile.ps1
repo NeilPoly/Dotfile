8fd9a16ec77ce6c50803ce6e12fc363f7d23480b
guxq@lzth-info <guxq@lzth-info.com>
2024-01-04 13:53:33 +0800

    Update Microsoft.PowerShell_profile.ps1

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -18,3 +18,33 @@ function which ($command) {
 
 # set directory background color(since 7.3 version)
 $PSStyle.FileInfo.Directory = "`e[38;2;255;255;255m"
+
+
+function set_proxy_variable {
+  $proxy = 'http://127.0.0.1:10809'
+
+    # temporary
+    $env:HTTP_PROXY = $proxy
+    $env:HTTPS_PROXY = $proxy
+
+    # forever
+    # [System.Environment]::SetEnvironmentVariable("HTTP_PROXY", $proxy, "User")
+    # [System.Environment]::SetEnvironmentVariable("HTTPS_PROXY", $proxy, "User")
+    
+    Write-Host "`n   OPEN powershell proxy channel!`n"
+}
+
+function unset_proxy_variable {
+    # temporary
+    Remove-Item env:HTTP_PROXY
+    Remove-Item env:HTTPS_PROXY
+
+    # forever
+    # [Environment]::SetEnvironmentVariable('http_proxy', $null, 'User')
+    # [Environment]::SetEnvironmentVariable('https_proxy', $null, 'User')
+
+    Write-Host "`n   CLOSE powershell proxy channel!`n"
+}
+
+Set-Alias proxy set_proxy_variable
+Set-Alias unproxy unset_proxy_variable
